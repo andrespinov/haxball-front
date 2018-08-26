1f07a100cedc7b1f42eb1e6f2d899982a4e44794
Andrés Pino Vallejo <andrespinovallejo@gmail.com>
2018-08-26 18:38:37 -0500

    Game service already made

--- src/app/game.service.ts
+++ src/app/game.service.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core';
+import { Headers, Http } from '@angular/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GameService {
+  private apiUrl = 'http://localhost:3000/player';
+  showAddGameBox:boolean = true;
 
-  constructor() { }
+  constructor(private http: Http) { }
+
+  getPlayers(): Promise<any>{
+    return this.http.get(this.apiUrl)
+               .toPromise()
+               .then(this.handleData)
+               .catch(this.handleError)
+    }
+  getPlayer(id:string): Promise<any>{
+    return this.http.get(this.apiUrl + id)
+                    .toPromise()
+                    .then(this.handleData)
+                    .catch(this.handleError)
+  }
+  createPlayer(player:any,socket:any): void{
+    socket.emit('addPlayer', player);
+  }
+  updatePlayer(player:any,socket:any):void{
+    socket.emit('updatePlayer', player);
+  }
+  deletePlayer(player:any,socket:any):void{
+    socket.emit('deletePlayer', player);
+  }
+
 }
